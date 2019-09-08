2a0be04344e3d8629f0da16186efdaee428ae496
Ubuntu <brianpenghe@gmail.com>
2019-09-08 20:31:18 +0000

    changed fastq file names

--- PlateTo10XFastq.sh
+++ PlateTo10XFastq.sh
@@ -7,7 +7,7 @@ for FID in $1/*R1.fastq
     		sed "s/\(@.*\)/\1 1:N:0:CCTACCAT+$BARCODE/" $FID | gzip -c >> Undetermined_S0_L001_R1_001.fastq.gz
     		grep "@" $FID | sed "s/$/ 1:N:0:CCTACCAT+$BARCODE\nCCTACCAT\n+\nZZZZZZZZ/" | gzip -c >> Undetermined_S0_L001_I1_001.fastq.gz
     		FID=$(sed 's/R1/R2/g' <<< $FID)
-    		sed "s/\(@.*\)/\1 2:N:0:CCTACCAT+$BARCODE/" $FID | gzip -c >> Undetermined_S0_L001_R2_001.fastq.gz
-    		grep "@" $FID | sed "s/$/ 2:N:0:CCTACCAT+$BARCODE\n$BARCODE\n+\nZZZZZZZZZZZZZZZZ/" | gzip -c >> Undetermined_S0_L001_I2_001.fastq.gz 
+    		sed "s/\(@.*\)/\1 2:N:0:CCTACCAT+$BARCODE/" $FID | gzip -c >> Undetermined_S0_L001_R3_001.fastq.gz
+    		grep "@" $FID | sed "s/$/ 2:N:0:CCTACCAT+$BARCODE\n$BARCODE\n+\nZZZZZZZZZZZZZZZZ/" | gzip -c >> Undetermined_S0_L001_R2_001.fastq.gz 
     		(( count++ ))
 	done
