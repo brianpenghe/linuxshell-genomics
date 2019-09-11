6c84dd46665bce4ed44b33e249ee4b00888bd7a7
Ubuntu <brianpenghe@gmail.com>
2019-09-11 11:24:54 +0000

    now CheckFastq.sh can catch truncation at the bottom of fastq files

--- CheckFastqs.sh
+++ CheckFastqs.sh
@@ -13,5 +13,10 @@ for FID in $1/*.fastq
 				echo $FID
 				cat $FID | paste - - - - | grep -v ^@ 
 		fi
+		if [ $(cat $FID | paste - - - - | awk -F"\t" '{ if (length($2) != length($4)) print $0 }' | wc -l) -gt 0 ]
+                        then
+                                echo $FID
+                                cat $FID | paste - - - - | awk -F"\t" '{ if (length($2) != length($4)) print $0 }' | wc -l
+                fi
 		count=$(($count+1))
 	done
