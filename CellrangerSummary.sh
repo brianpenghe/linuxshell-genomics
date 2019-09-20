04bf47a6cbd0c9b10ae499fa2e539af2983ffd67
Ubuntu <brianpenghe@gmail.com>
2019-09-20 12:45:09 +0000

    added a script to assemble summary.csv files

---
+++ CellrangerSummary.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#CellrangerSummary.sh 5891STDY80386{51..67}
+echo -n "sample," > summary.csv
+head -1 $1/outs/summary.csv >> summary.csv
+for sample in "$@"
+    do
+         echo -n $sample >> summary.csv; tail -1 $sample/outs/summary.csv >> summary.csv
+    done
+
