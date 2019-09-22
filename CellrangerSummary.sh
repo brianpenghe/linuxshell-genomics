65a83dcef14d0da61cf7625fe9156a5edda07813
Ubuntu <brianpenghe@gmail.com>
2019-09-22 17:56:04 +0000

    [important] now parse csv better, using csvtool and csvkit

--- CellrangerSummary.sh
+++ CellrangerSummary.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 #CellrangerSummary.sh 5891STDY80386{51..67}
-cat <(echo "sample") <(head -1 $1/outs/summary.csv | tr , '\n') > summary.csv
+csvtool transpose $1/outs/*summary.csv | csvcut -c 1 > summary.csv
 for sample in "$@"
     do
-        paste <(cat <(echo "sample") <(head -1 $sample/outs/summary.csv | tr , '\n')) \
-              <(cat <(echo $sample)  <(tail -1 $sample/outs/summary.csv | tr , '\n')) > temp2
-        join -1 1 -2 1 summary.csv temp2 > summary2.csv
-        mv summary2.csv summary.csv
+        csvtool transpose $sample/outs/*summary.csv > tempfilesforcsvtool.$sample
+        csvjoin -c 1 summary.csv tempfilesforcsvtool.$sample > temp2
+        mv temp2 summary.csv
     done
+
+csvformat -T summary.csv > summary.tsv
+rm summary.csv
