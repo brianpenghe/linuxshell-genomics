177117470cf0788816bd3695666e7fa3fe3b0497
Ubuntu <brianpenghe@gmail.com>
2019-10-23 10:29:48 +0000

    added a script to do VDJ

---
+++ CellRangerVDJ.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -x
+CPU=22
+CELLRANGER=/home/ubuntu/tools/cellranger-3.0.2/cellranger
+REFSEQ=/home/ubuntu/refseq/refdata-cellranger-vdj-GRCh38-alts-ensembl-3.1.0
+
+### Non-CITE Samples
+for PREFIX in 5891STDY80623{33..48}
+    do
+        $CELLRANGER vdj --localcores $CPU --id=$PREFIX --reference=$REFSEQ \
+--fastqs=/mnt/190911DaweiLung/fastq-$PREFIX \
+--sample=$PREFIX
+rm -r ./$PREFIX/SC_* *.tgz _* 
+done
+
