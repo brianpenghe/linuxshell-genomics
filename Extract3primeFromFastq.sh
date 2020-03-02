ad17135e071badb35d8f4af6b9835eff51e4117c
Peng He <phe@myogenin.caltech.edu>
2020-03-02 04:00:10 -0800

    modified the bug

--- Extract3primeFromFastq.sh
+++ Extract3primeFromFastq.sh
@@ -1,3 +1,3 @@
-#!/bin/sh
+#!/bin/bash
 #usage:  ./Extract3primeFromFastq.sh FastqFile.fq 30 > 3prime.30.fq
-paste <(cat $1 | paste - - | cut -f1 | tr "\t" "\n") <(cat $1 | paste - - | cut -f2 | tr "\t" "\n" | grep -o '.\{$2\}$' ) | tr "\t" "\n"
+paste <(cat $1 | paste - - | cut -f1 | tr "\t" "\n") <(cat $1 | paste - - | cut -f2 | tr "\t" "\n" | grep -o '.\{'$2'\}$' ) | tr "\t" "\n"
