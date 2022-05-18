17d2cdc81f98412d1ec58cb1379c78524469942a
Ubuntu <brianpenghe@gmail.com>
2022-05-18 14:59:46 +0000

    now changed Z to J for newer cellrangerATAC

--- PlateTo10XFastq.sh
+++ PlateTo10XFastq.sh
@@ -6,9 +6,9 @@ for FID in $1/*R1.fastq
 	do
 		BARCODE=`head -n $count $2 | tail -n 1`
     		sed "s/\(@.*\)/\1 1:N:0:CCTACCAT+$BARCODE/" $FID | gzip -c >> Undetermined_S0_L001_R1_001.fastq.gz
-    		grep "@" $FID | sed "s/$/ 1:N:0:CCTACCAT+$BARCODE\nCCTACCAT\n+\nZZZZZZZZ/" | gzip -c >> Undetermined_S0_L001_I1_001.fastq.gz
+    		grep "@" $FID | sed "s/$/ 1:N:0:CCTACCAT+$BARCODE\nCCTACCAT\n+\nJJJJJJJJ/" | gzip -c >> Undetermined_S0_L001_I1_001.fastq.gz
     		FID=$(sed 's/R1/R2/g' <<< $FID)
     		sed "s/\(@.*\)/\1 2:N:0:CCTACCAT+$BARCODE/" $FID | gzip -c >> Undetermined_S0_L001_R3_001.fastq.gz
-    		grep "@" $FID | sed "s/$/ 2:N:0:CCTACCAT+$BARCODE\n$BARCODE\n+\nZZZZZZZZZZZZZZZZ/" | gzip -c >> Undetermined_S0_L001_R2_001.fastq.gz 
+    		grep "@" $FID | sed "s/$/ 2:N:0:CCTACCAT+$BARCODE\n$BARCODE\n+\nJJJJJJJJJJJJJJJJ/" | gzip -c >> Undetermined_S0_L001_R2_001.fastq.gz 
     		(( count++ ))
 	done
